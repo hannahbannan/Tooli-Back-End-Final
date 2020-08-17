547c09c2565035a0e0209e90d2495eabecde18b8
Hannah Bannan <hannahbannan@macbook-pro-2.lan>
2020-08-17 16:58:58 -0700

    moved cors to config.ru

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,20 +1,21 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins 'http://localhost:3001'
+#       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+#     end
 
 
-    # Production origin
-    allow do
-        origins 'https://tooli.netlify.app'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+#     # Production origin
+#     allow do
+#         origins 'https://tooli.netlify.app'
+#         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+#     end
 
-    # allow do
-    #     origins '*'
-    #     resource '*',
-    #       headers: :any,
-    #       methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    #   end
-end
+
+#     # allow do
+#     #     origins '*'
+#     #     resource '*',
+#     #       headers: :any,
+#     #       methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+#     #   end
+# end
